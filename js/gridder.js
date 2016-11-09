babcb87588a79e6739d7617779db59e9ff075c60
pmn1212 <pmn33@n2-37-62.dhcp.cabrini.edu>
2016-11-09 11:37:04 -0500

    Initial Commit

---
+++ js/gridder.js
@@ -0,0 +1,9 @@
+// Loads the JQuery Library //
+$(document).ready(function() {
+createGridder = function() {
+  document.body.appendChild(
+    document.createElement('script'))
+    .src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/788824/960.gridder.js';
+}
+  }); // End of ready function.
+
